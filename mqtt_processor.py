3d2bfe1a820c20105eee1217cc2f163438c02ec0
redman <redman@redman.hu>
2017-08-20 13:11:19 +0200

    MQTT publish message for switch changed to retained

--- mqtt_processor.py
+++ mqtt_processor.py
@@ -5,7 +5,7 @@
 # msg should be an mqtt message object
 def process (msg, client):    
     if msg.topic == "/home/users/away":
-	client.publish("/home/device/bedroom/sonoff", "0", 0, False);
+	client.publish("/home/device/bedroom/sonoff", "0", 0, True);
 	
     if msg.topic == "/home/users/home":
-	client.publish("/home/device/bedroom/sonoff", "1", 0, False);
+	client.publish("/home/device/bedroom/sonoff", "1", 0, True);
